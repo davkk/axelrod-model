a6e15eb6c8cdf6f4d14e09725278e1516f342e9f
davkk <dadulerk@gmail.com>
2024-05-16 20:39:56 +0200

    feat: working model simulation

--- main.ts
+++ main.ts
@@ -6,41 +6,86 @@ if (ctx == null) throw new Error("Context could not be created");
 
 const { width } = canvas.getBoundingClientRect();
 
-const cellSize = 8;
+const cellSize = 16;
 const cols = width / cellSize;
 
 let running = true;
-let numFeatures = 3;
-let numTraits = 3;
+let numFeatures = 4;
+let numTraits = 5;
 
-function getCells() {
+const cells = Array.from({ length: cols }, () => {
     return Array.from({ length: cols }, () => {
-        return Array.from({ length: cols }, () => {
-            return Array.from({ length: numFeatures }, () => {
-                return Math.floor(Math.random() * numTraits);
-            });
+        return Array.from({ length: numFeatures }, () => {
+            return randInt(numTraits);
         });
     });
+});
+
+function randInt(max: number) {
+    return Math.floor(Math.random() * max);
 }
 
-function drawGrid(element: HTMLCanvasElement) {
-    const ctx = element.getContext("2d");
-    if (ctx == null) throw new Error("Could not get context");
+function mod(a: number, b: number) {
+    return ((a % b) + b) % b;
+}
 
-    ctx.clearRect(0, 0, element.width, element.height);
+function mapFeaturesToColor(features: number[]) {
+    const hue = features.reduce((a, b) => a + b, 0) / features.length;
+    const saturation = 0.8;
+    const lightness = 0.5;
+    const hslColor = `hsl(${hue * 360}, ${saturation * 100}%, ${lightness * 100}%)`;
+    return hslColor;
+}
 
-    for (let row = 0; row < cols; ++row) {
-        for (let col = 0; col < cols; ++col) {
-            ctx.fillStyle = "white";
-            ctx.fillRect(row * cellSize, col * cellSize, cellSize, cellSize);
+function drawGrid() {
+    if (!canvas || !ctx) return;
+
+    ctx.clearRect(0, 0, width, width);
+
+    for (let y = 0; y < cols; ++y) {
+        for (let x = 0; x < cols; ++x) {
+            ctx.fillStyle = mapFeaturesToColor(cells[y][x]);
+            ctx.fillRect(y * cellSize, x * cellSize, cellSize, cellSize);
         }
     }
 }
 
+const directions = [
+    [-1, 0],
+    [1, 0],
+    [0, 1],
+    [0, -1],
+];
+
 function simulate() {
-    for (let i = 0; i < cols; ++i) {
-        const cell1 = Math.floor(Math.random() * cols);
-        const cell2 = Math.floor(Math.random() * cols);
+    for (let step = 0; step < 1000000; ++step) {
+        const y = randInt(cols);
+        const x = randInt(cols);
+        const [dy, dx] = directions[randInt(directions.length)];
+
+        const cell1 = cells[y][x];
+        const cell2 = cells[mod(y + dy, cols)][mod(x + dx, cols)];
+
+        let sameFeatures = 0;
+        const availableFeatures = [];
+        for (let idx = 0; idx < numFeatures; ++idx) {
+            if (cell1[idx] === cell2[idx]) {
+                sameFeatures++;
+            } else {
+                availableFeatures.push(idx);
+            }
+        }
+
+        if (
+            sameFeatures < numFeatures &&
+            Math.random() < sameFeatures / numFeatures
+        ) {
+            const randFeature = randInt(availableFeatures.length);
+            cells[y][x][randFeature] = cell2[randFeature];
+        }
     }
-    setTimeout(simulate, 250);
+    drawGrid();
+    requestAnimationFrame(simulate);
 }
+
+simulate();
